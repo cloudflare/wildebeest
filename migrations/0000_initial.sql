b7a9685ca545bd1603f5221049bbfe9594107398
Sven Sauleau <sven@cloudflare.com>
2022-12-05 20:43:36 +0000

    ETI-917: add database and initial migration

---
+++ migrations/0000_initial.sql
@@ -0,0 +1,49 @@
+-- Migration number: 0000 	 2022-12-05T20:27:34.391Z
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS actors (
+  id TEXT PRIMARY KEY,
+  type TEXT NOT NULL,
+  privkey TEXT,
+  pubkey TEXT,
+  cdate DATETIME NOT NULL DEFAULT (datetime()),
+  properties TEXT NOT NULL DEFAULT (json_object())
+);
+
+CREATE TABLE IF NOT EXISTS actor_following (
+  id TEXT PRIMARY KEY,
+  object_id TEXT NOT NULL,
+  actor_id TEXT NOT NULL,
+  cdate DATETIME NOT NULL DEFAULT (datetime()),
+
+  FOREIGN KEY(actor_id)  REFERENCES actors(id),
+  FOREIGN KEY(object_id) REFERENCES objects(object_id)
+);
+
+CREATE TABLE IF NOT EXISTS actor_followers (
+  id TEXT PRIMARY KEY,
+  object_id TEXT NOT NULL,
+  actor_id TEXT NOT NULL,
+  cdate DATETIME NOT NULL DEFAULT (datetime()),
+
+  FOREIGN KEY(actor_id)  REFERENCES actors(id),
+  FOREIGN KEY(object_id) REFERENCES objects(object_id)
+);
+
+CREATE TABLE IF NOT EXISTS objects (
+  id TEXT PRIMARY KEY,
+  type TEXT NOT NULL,
+  cdate DATETIME NOT NULL DEFAULT (datetime()),
+  properties TEXT NOT NULL DEFAULT (json_object())
+);
+
+CREATE TABLE IF NOT EXISTS inbox_objects (
+  id TEXT PRIMARY KEY,
+  actor_id TEXT NOT NULL,
+  object_id TEXT NOT NULL,
+  cdate DATETIME NOT NULL DEFAULT (datetime()),
+
+  FOREIGN KEY(actor_id)  REFERENCES actors(id),
+  FOREIGN KEY(object_id) REFERENCES objects(object_id)
+);
