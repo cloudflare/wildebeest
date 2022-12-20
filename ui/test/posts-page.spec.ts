7ae8d277d3194e6348514888ba28de4a804d4472
Pete Bacon Darwin <pete@bacondarwin.com>
2022-12-20 19:56:54 +0000

    fixup! Add direct upload deployment scripts and instructions

--- ui/test/posts-page.spec.ts
+++ ui/test/posts-page.spec.ts
@@ -2,9 +2,25 @@ import { fetch } from 'undici'
 
 describe('Posts timeline page', () => {
 	it('should display a list of statuses', async () => {
-		const response = await fetch('http://localhost:6868/')
-		expect(response.status).toBe(200)
-		const body = await response.text()
-		expect(body).toContain('This is significant ...')
+		try {
+			console.log('127.0.0.1')
+			const response = await fetch('http://127.0.0.1:6868/')
+			expect(response.status).toBe(200)
+			const body = await response.text()
+			expect(body).toContain('This is significant ...')
+		} catch (e) {
+			const error = e as Error
+			console.error(error.stack)
+		}
+		try {
+			console.log('0.0.0.0')
+			const response = await fetch('http://0.0.0.0:6868/')
+			expect(response.status).toBe(200)
+			const body = await response.text()
+			expect(body).toContain('This is significant ...')
+		} catch (e) {
+			const error = e as Error
+			console.error(error.stack)
+		}
 	})
 })
