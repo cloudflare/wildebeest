93ed83f3731c27391a72c551314c80771865bc2d
Pete Bacon Darwin <pete@bacondarwin.com>
2022-12-22 09:54:25 +0000

    Fix up the ui test to match the dummy data

--- frontend/test/posts-page.spec.ts
+++ frontend/test/posts-page.spec.ts
@@ -2,10 +2,9 @@ import { fetch } from 'undici'
 
 describe('Posts timeline page', () => {
 	it('should display a list of statuses', async () => {
-		console.log('0.0.0.0')
-		const response = await fetch('http://0.0.0.0:6868/')
+		const response = await fetch('http://0.0.0.0:6868/explore/')
 		expect(response.status).toBe(200)
 		const body = await response.text()
-		expect(body).toContain('There were three men came out of the West')
+		expect(body).toContain('It is that time of the year. The Wreath of Khan.')
 	})
 })
