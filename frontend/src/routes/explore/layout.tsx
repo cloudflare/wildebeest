4a269d6ab592d8b6ab2d49840eed2e16b95e60d2
James Culveyhouse <jculveyhouse@cloudflare.com>
2022-12-21 22:30:37 -0600

    MOW-26: Adding active styling to explore nav

--- frontend/src/routes/explore/layout.tsx
+++ frontend/src/routes/explore/layout.tsx
@@ -1,8 +1,45 @@
 import { component$, Slot, useStylesScoped$ } from '@builder.io/qwik'
+import { useLocation } from '@builder.io/qwik-city'
 import styles from './layout.scss?inline'
+
+type LinkConfig = {
+	linkText: string
+	linkTarget: string
+}
 
 export default component$(() => {
 	useStylesScoped$(styles)
+	const location = useLocation()
+
+	const renderNavLink = ({ linkText, linkTarget }: LinkConfig) => {
+		let classList = 'no-decoration text-bold text-slate-200 py-4'
+
+		// Color the active link indigo. Qwik pathnames always include a trailing `/`
+		if (location.pathname === `${linkTarget}/`) {
+			classList += ' active'
+		}
+
+		return (
+			<a href={linkTarget} class={classList}>
+				{linkText}
+			</a>
+		)
+	}
+
+	const links = [
+		{
+			linkText: 'Posts',
+			linkTarget: '/explore',
+		},
+		{
+			linkText: 'Hashtags',
+			linkTarget: '/explore/tags',
+		},
+		{
+			linkText: 'News',
+			linkTarget: '/explore/links',
+		},
+	]
 
 	return (
 		<div class="container">
@@ -12,17 +49,7 @@ export default component$(() => {
 					<span>Explore</span>
 				</h2>
 			</div>
-			<div class="bg-slate-900 flex justify-around">
-				<a href="/explore" class="no-decoration text-bold text-slate-200 py-4">
-					Posts
-				</a>
-				<a href="/explore/tags" class="no-decoration text-bold text-slate-400 py-4" href="/explore/tags">
-					Hashtags
-				</a>
-				<a class="no-decoration text-bold text-slate-400 py-4" href="/explore/links">
-					News
-				</a>
-			</div>
+			<div class="bg-slate-900 flex justify-around">{links.map((link) => renderNavLink(link))}</div>
 			<div class="content-container">
 				<Slot />
 			</div>
