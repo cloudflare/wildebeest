e49cfb4be8a8fb545806731c75446175c2ffb245
Sven Sauleau <sven@cloudflare.com>
2022-12-24 17:12:24 +0000

    support image document

--- backend/src/media/index.ts
+++ backend/src/media/index.ts
@@ -5,33 +5,43 @@ import type { Object } from 'wildebeest/backend/src/activitypub/objects'
 
 export function fromObject(obj: Object): MediaAttachment {
 	if (obj.type === IMAGE) {
-		return {
-			url: new URL(obj.url),
-			id: obj.mastodonId!,
-			preview_url: new URL(obj.url),
-			type: 'image',
-			meta: {
-				original: {
-					width: 640,
-					height: 480,
-					size: '640x480',
-					aspect: 1.3333333333333333,
-				},
-				small: {
-					width: 461,
-					height: 346,
-					size: '461x346',
-					aspect: 1.3323699421965318,
-				},
-				focus: {
-					x: -0.27,
-					y: 0.51,
-				},
-			},
-			description: 'test media description',
-			blurhash: 'UFBWY:8_0Jxv4mx]t8t64.%M-:IUWGWAt6M}',
+		return fromObjectImage(obj)
+	} else if (obj.type === 'Document') {
+		if (obj.mediaType === 'image/jpeg') {
+			return fromObjectImage(obj)
+		} else {
+			throw new Error(`unsupported media type ${obj.type}: ${JSON.stringify(obj)}`)
 		}
 	} else {
-		throw new Error('unsupported media type: ' + obj.type)
+		throw new Error(`unsupported media type ${obj.type}: ${JSON.stringify(obj)}`)
+	}
+}
+
+function fromObjectImage(obj: Object): MediaAttachment {
+	return {
+		url: new URL(obj.url),
+		id: obj.mastodonId || obj.url.toString(),
+		preview_url: new URL(obj.url),
+		type: 'image',
+		meta: {
+			original: {
+				width: 640,
+				height: 480,
+				size: '640x480',
+				aspect: 1.3333333333333333,
+			},
+			small: {
+				width: 461,
+				height: 346,
+				size: '461x346',
+				aspect: 1.3323699421965318,
+			},
+			focus: {
+				x: -0.27,
+				y: 0.51,
+			},
+		},
+		description: 'test media description',
+		blurhash: 'UFBWY:8_0Jxv4mx]t8t64.%M-:IUWGWAt6M}',
 	}
 }
