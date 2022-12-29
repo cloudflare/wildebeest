b7cf11308072c7ac7df00433faec7862f9758276
Dario Piotrowicz <dario@cloudflare.com>
2022-12-29 23:19:55 +0100

    apply sticky header to all pages

--- frontend/src/routes/public/local/index.tsx
+++ frontend/src/routes/public/local/index.tsx
@@ -21,9 +21,11 @@ export default component$(() => {
 			onResolved={(statuses) => {
 				return (
 					<>
-						<div class="rounded-t bg-slate-700 p-4 flex items-center">
-							<i class="fa fa-users fa-fw mr-3 text-slate-100" />
-							<span>Local timeline</span>
+						<div class="header-wrapper">
+							<div class="rounded-t bg-slate-700 p-4 flex items-center">
+								<i class="fa fa-users fa-fw mr-3 text-slate-100" />
+								<span>Local timeline</span>
+							</div>
 						</div>
 						{statuses.map((status) => (
 							<Status status={status} />
