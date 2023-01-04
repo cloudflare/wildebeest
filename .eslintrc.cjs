c9d00ad16650b689cf4c85f601d0ce9be1b18a18
Dario Piotrowicz <dario@cloudflare.com>
2023-01-04 09:38:33 +0100

    add lint:be script and make it pass

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,4 +11,34 @@ module.exports = {
 	},
 	plugins: ['@typescript-eslint'],
 	root: true,
+	rules: {
+		/*
+			Note: the following rules have been set to off so that linting
+				  can pass with the current code, but we need to gradually
+				  re-enable most of them
+		*/
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-argument': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'@typescript-eslint/restrict-plus-operands': 'off',
+		'no-constant-condition': 'off',
+		'@typescript-eslint/await-thenable': 'off',
+		'prefer-const': 'off',
+		'@typescript-eslint/require-await': 'off',
+		'@typescript-eslint/restrict-template-expressions': 'off',
+		'@typescript-eslint/no-misused-promises': 'off',
+		'no-var': 'off',
+		'@typescript-eslint/no-unsafe-return': 'off',
+		'@typescript-eslint/no-unnecessary-type-assertion': 'off',
+		'no-console': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/no-empty-interface': 'off',
+	},
 }
