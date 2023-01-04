4ac2d4a8fc155003e672cc9ee8ceaf2203b6f5e5
Dario Piotrowicz <dario@cloudflare.com>
2023-01-04 14:25:47 +0100

    apply a single DocumentHead to the root layout component

--- frontend/src/routes/explore/index.tsx
+++ frontend/src/routes/explore/index.tsx
@@ -1,5 +1,4 @@
 import { component$ } from '@builder.io/qwik'
-import { DocumentHead } from '@builder.io/qwik-city'
 import Explore from './Explore'
 
 export { statusesLoader } from './Explore'
@@ -7,13 +6,3 @@ export { statusesLoader } from './Explore'
 export default component$(() => {
 	return <Explore />
 })
-
-export const head: DocumentHead = {
-	title: 'Wildebeest (Mastodon on Cloudflare)',
-	meta: [
-		{
-			name: 'description',
-			content: 'A frontend for a mastodon server deployed on Cloudflare.',
-		},
-	],
-}
