41846f2d4209a77ce11e61884eacdf7b98735d35
Sven Sauleau <sven@cloudflare.com>
2023-01-12 10:57:35 +0000

    MOW-98: add optional sentry

--- backend/src/middleware/error.ts
+++ backend/src/middleware/error.ts
@@ -1,12 +1,19 @@
 import { internalServerError } from '../errors'
+import type { Env } from 'wildebeest/backend/src/types/env'
+import { initSentry } from 'wildebeest/backend/src/utils/sentry'
 
 /**
  * A Pages middleware function that logs errors to the console and responds with 500 errors and stack-traces.
  */
-export async function errorHandling(context: EventContext<unknown, any, any>) {
+export async function errorHandling(context: EventContext<Env, any, any>) {
+	const sentry = initSentry(context.request, context.env, context)
+
 	try {
 		return await context.next()
 	} catch (err: any) {
+		if (sentry !== null) {
+			sentry.captureException(err)
+		}
 		console.error(err)
 		return internalServerError()
 	}
---
+++ backend/src/utils/sentry.ts
@@ -0,0 +1,30 @@
+import { Toucan } from 'toucan-js'
+import type { Env } from 'wildebeest/backend/src/types/env'
+
+export function initSentry(request: Request, env: Env, context: any) {
+	if (env.SENTRY_DSN === '') {
+		return null
+	}
+
+	const headers: any = {}
+
+	if (env.SENTRY_ACCESS_CLIENT_ID !== '' && env.SENTRY_ACCESS_CLIENT_SECRET !== '') {
+		headers['CF-Access-Client-ID'] = env.SENTRY_ACCESS_CLIENT_ID
+		headers['CF-Access-Client-Secret'] = env.SENTRY_ACCESS_CLIENT_SECRET
+	}
+
+	const sentry = new Toucan({
+		dsn: env.SENTRY_DSN,
+		context,
+		request,
+		transportOptions: { headers },
+	})
+	const colo = request.cf && request.cf.colo ? request.cf.colo : 'UNKNOWN'
+	sentry.setTag('colo', colo)
+
+	// cf-connecting-ip should always be present, but if not we can fallback to XFF.
+	const ipAddress = request.headers.get('cf-connecting-ip') || request.headers.get('x-forwarded-for')
+	const userAgent = request.headers.get('user-agent') || ''
+	sentry.setUser({ ip: ipAddress, userAgent: userAgent, colo: colo })
+	return sentry
+}
