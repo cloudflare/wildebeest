5929db73f8cc74ba2aedb8a7ce64e12f980bcc8d
Sven Sauleau <sven@cloudflare.com>
2023-02-03 11:57:43 +0000

    MOW-130: NodeInfo 2.0 and 2.1
    Closes https://github.com/cloudflare/wildebeest/issues/160

--- config/versions.ts
+++ config/versions.ts
@@ -3,7 +3,7 @@ import * as packagejson from '../package.json'
 // https://github.com/mastodon/mastodon/blob/main/CHANGELOG.md
 const MASTODON_API_VERSION = '4.0.2'
 
-const WILDEBEEST_VERSION = packagejson.version
+export const WILDEBEEST_VERSION = packagejson.version
 
 export function getVersion(): string {
 	return `${MASTODON_API_VERSION} (compatible; Wildebeest ${WILDEBEEST_VERSION})`
