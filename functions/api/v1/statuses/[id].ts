13b827dcfa0fbb39a2f4838784f6f27d5375b550
Sven Sauleau <sven@cloudflare.com>
2023-02-08 17:30:00 +0000

    MOW-139: support federated Note deletion

--- functions/api/v1/statuses/[id].ts
+++ functions/api/v1/statuses/[id].ts
@@ -10,7 +10,7 @@ import type { ContextData } from 'wildebeest/backend/src/types/context'
 import { getMastodonStatusById, toMastodonStatusFromObject } from 'wildebeest/backend/src/mastodon/status'
 import type { Env } from 'wildebeest/backend/src/types/env'
 import * as errors from 'wildebeest/backend/src/errors'
-import { getObjectByMastodonId } from 'wildebeest/backend/src/activitypub/objects'
+import { getObjectByMastodonId, deleteObject } from 'wildebeest/backend/src/activitypub/objects'
 import { urlToHandle } from 'wildebeest/backend/src/utils/handle'
 import { deliverFollowers } from 'wildebeest/backend/src/activitypub/deliver'
 import type { Queue, DeliverMessageBody } from 'wildebeest/backend/src/types/queue'
@@ -48,31 +48,6 @@ export async function handleRequestGet(db: D1Database, id: UUID, domain: string)
 	return new Response(JSON.stringify(status), { headers })
 }
 
-// TODO: eventually use SQLite's `ON DELETE CASCADE` but requires writing the DB
-// schema directly into D1, which D1 disallows at the moment.
-// Some context at: https://stackoverflow.com/questions/13150075/add-on-delete-cascade-behavior-to-an-sqlite3-table-after-it-has-been-created
-async function deleteNote(db: D1Database, note: Note) {
-	const nodeId = note.id.toString()
-	const batch = [
-		db.prepare('DELETE FROM outbox_objects WHERE object_id=?').bind(nodeId),
-		db.prepare('DELETE FROM inbox_objects WHERE object_id=?').bind(nodeId),
-		db.prepare('DELETE FROM actor_notifications WHERE object_id=?').bind(nodeId),
-		db.prepare('DELETE FROM actor_favourites WHERE object_id=?').bind(nodeId),
-		db.prepare('DELETE FROM actor_reblogs WHERE object_id=?').bind(nodeId),
-		db.prepare('DELETE FROM actor_replies WHERE object_id=?1 OR in_reply_to_object_id=?1').bind(nodeId),
-		db.prepare('DELETE FROM idempotency_keys WHERE object_id=?').bind(nodeId),
-		db.prepare('DELETE FROM objects WHERE id=?').bind(nodeId),
-	]
-
-	const res = await db.batch(batch)
-
-	for (let i = 0, len = res.length; i < len; i++) {
-		if (!res[i].success) {
-			throw new Error('SQL error: ' + res[i].error)
-		}
-	}
-}
-
 export async function handleRequestDelete(
 	db: D1Database,
 	id: UUID,
@@ -95,7 +70,7 @@ export async function handleRequestDelete(
 		return errors.statusNotFound(id)
 	}
 
-	await deleteNote(db, obj)
+	await deleteObject(db, obj)
 
 	// FIXME: deliver a Delete message to our peers
 	const activity = activities.create(domain, connectedActor, obj)
