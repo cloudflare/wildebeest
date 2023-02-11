3409a3e32d56e9f3fdab56dbe4a5894fff307d0a
DataDrivenMD <116459476+DataDrivenMD@users.noreply.github.com>
2023-02-10 21:30:48 -0800

    Define MastodonInstance types

---
+++ backend/src/types/instance.ts
@@ -0,0 +1,66 @@
+import type { MastodonAccount } from './account'
+
+// https://docs.joinmastodon.org/entities/Instance/
+// https://github.com/mastodon/mastodon-ios/blob/develop/MastodonSDK/Sources/MastodonSDK/Entity/Mastodon%2BEntity%2BInstance.swift
+// https://github.com/mastodon/mastodon-android/blob/master/mastodon/src/main/java/org/joinmastodon/android/model/Instance.java
+export interface MastodonInstance {
+	uri: string
+  title: string
+  description: string
+  short_description: string
+  email: string
+  version?: string
+  languages?: Array<string>
+  registrations?: boolean
+  approval_required?: boolean
+  invites_enabled?: boolean
+	urls?: InstanceURL
+  statistics?: Statistics
+  thumbnail?: string
+  contact_account?: MastodonAccount
+  rules?: Array<Rule>
+  configuration?: Configuration
+}
+
+export interface InstanceURL {
+  streaming_api: string
+}
+
+export type Statistics = {
+	user_count: number
+  status_count: number
+  domain_count: number
+}
+
+export type Rule = {
+	id: string
+  text: string
+}
+
+export type Configuration = {
+  statuses?: StatusesConfiguration
+  media_attachments?: MediaAttachmentsConfiguration
+  polls?: PollsConfiguration
+}
+
+export type StatusesConfiguration = {
+  max_characters: number
+  max_media_attachments: number
+  characters_reserved_per_url: number
+}
+
+export type MediaAttachmentsConfiguration = {
+  supported_mime_types: Array<string>
+  image_size_limit: number
+  image_matrix_limit: number
+  video_size_limit: number
+  video_frame_rate_limit: number
+  video_matrix_limit: number
+}
+
+export type PollsConfiguration = {
+  max_options: number
+  max_characters_per_option: number
+  min_expiration: number
+  max_expiration: number
+}
