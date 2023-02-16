caf79ef0f39a5b7f61494c68388d7dc86dc29ba5
Dario Piotrowicz <dario@cloudflare.com>
2023-02-16 16:22:36 +0000

    make sure time indicator doesn't wrap

--- frontend/src/components/Status/index.tsx
+++ frontend/src/components/Status/index.tsx
@@ -31,7 +31,7 @@ export default component$((props: Props) => {
 				<Link class="no-underline" href={statusUrl}>
 					<div class="text-wildebeest-500 flex items-baseline">
 						<i style={{ height: '0.75rem', width: '0.75rem' }} class="fa fa-xs fa-globe w-3 h-3" />
-						<span class="ml-2 text-sm hover:underline">{formatTimeAgo(new Date(status.created_at))}</span>
+						<span class="ml-2 text-sm hover:underline min-w-max">{formatTimeAgo(new Date(status.created_at))}</span>
 					</div>
 				</Link>
 			</div>
