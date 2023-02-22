112133f8727931301aea9e5b85bc3f994026ca09
Dario Piotrowicz <dario@cloudflare.com>
2023-02-22 13:11:08 +0000

    enable playwright testing on mobile viewports

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,18 +66,18 @@ const config: PlaywrightTestConfig = {
 		},
 
 		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: {
-		//     ...devices['Pixel 5'],
-		//   },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: {
-		//     ...devices['iPhone 12'],
-		//   },
-		// },
+		{
+			name: 'Mobile Chrome',
+			use: {
+				...devices['Pixel 5'],
+			},
+		},
+		{
+			name: 'Mobile Safari',
+			use: {
+				...devices['iPhone 12'],
+			},
+		},
 
 		/* Test against branded browsers. */
 		// {
