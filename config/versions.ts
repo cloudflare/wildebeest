79c3b61c812fea16596646eb59ddfebdf9c71b13
Jorge Caballero (DataDrivenMD) <116459476+DataDrivenMD@users.noreply.github.com>
2023-03-08 10:31:45 -0800

    Reverting breaking change to `getVersion()`

--- config/versions.ts
+++ config/versions.ts
@@ -4,3 +4,7 @@ import * as packagejson from '../package.json'
 export const MASTODON_API_VERSION = '4.0.2'
 
 export const WILDEBEEST_VERSION = packagejson.version
+
+export function getVersion(): string {
+	return `${MASTODON_API_VERSION} (compatible; Wildebeest ${WILDEBEEST_VERSION})`
+}
