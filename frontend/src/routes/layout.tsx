9fe622cddfc0e6a4d69d00849d01c0811a4a9f61
Sven Sauleau <sven@cloudflare.com>
2023-03-08 09:46:21 +0000

    add client for the UI

--- frontend/src/routes/layout.tsx
+++ frontend/src/routes/layout.tsx
@@ -6,12 +6,12 @@ type AuthLoaderData = {
 	isAuthorized: boolean
 }
 
-export const authLoader = loader$<Promise<AuthLoaderData>>(async ({ platform, request }) => {
+export const authLoader = loader$<Promise<AuthLoaderData>>(async ({ platform }) => {
 	const isAuthorized = platform.data.connectedActor !== null
-	// FIXME(sven): remove hardcoded value
+	// defined in migrations/0010_add_ui_client.sql
 	const UI_CLIENT_ID = '924801be-d211-495d-8cac-e73503413af8'
 	const params = new URLSearchParams({
-		redirect_uri: request.url,
+		redirect_uri: '/',
 		response_type: 'code',
 		client_id: UI_CLIENT_ID,
 		scope: 'all',
