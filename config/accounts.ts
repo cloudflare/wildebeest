2dda0735fe25a6b74a6a76bc4289b92b33606421
wellwich <76580263+wellwich@users.noreply.github.com>
2023-07-04 17:08:06 +0900

    Update accounts.ts

--- config/accounts.ts
+++ config/accounts.ts
@@ -1,5 +1,5 @@
 import type { DefaultImages } from '../backend/src/types/configs'
 export const defaultImages: DefaultImages = {
-	avatar: 'https://api.cloudflare.com/client/v4/accounts/50b08df319531cd31d6820a2fd029c87/images/v1/b7963f9d-134a-434a-2570-165ac8b03700',
+	avatar: 'https://api.cloudflare.com/client/v4/accounts/50b08df319531cd31d6820a2fd029c87/images/v1/b7963f9d-134a-434a-2570-165ac8b03700/avatar',
 	header: 'https://imagedelivery.net/NkfPDviynOyTAOI79ar_GQ/b24caf12-5230-48c4-0bf7-2f40063bd400/header',
 }
